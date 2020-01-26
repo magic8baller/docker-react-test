89d80993bdb21a0a4b73476232539f616bde22cb
magic8baller <lturis@gmail.com>
2020-01-26 15:36:22 -0500

    add tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,8 @@ function App () {
 			<h2>
 				Guten Tag!!! ?
         </h2>
-			<p> docker run -v /app/node_modules -p 1234:1234 -v $(pwd):/app 10a750b5f761</p>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-        </a>
+			<p>wooo</p>
+=
 		</div>
 	);
 }
