e001eff6654d80a194226ab36f0212a69917be03
magic8baller <lturis@gmail.com>
2020-01-26 15:12:57 -0500

    volumes with docker run

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,9 @@ function App () {
 	return (
 		<div className="App">
 			<h2>
-				Guten Tag!!!
+				Guten Tag!!! ?
         </h2>
+			<p> docker run -v /app/node_modules -p 1234:1234 -v $(pwd):/app 10a750b5f761</p>
 			<a
 				className="App-link"
 				href="https://reactjs.org"
